2256a1dc056fcbfb664c74a8ef97c5c562031c02
Justin <justin.reissig@icloud.com>
2022-08-07 00:46:29 +0200

    setup the server

--- server.js
+++ server.js
@@ -1,3 +1,14 @@
+// Just the main shit ya know
+const { Client, GatewayIntentBits } = require('discord.js')
 const { token } = require('./config.json')
 
-console.log(token)
+// New client instance
+const client = new Client({ intents: [GatewayIntentBits.Guilds] })
+
+// When client ist ready...
+client.once('ready', () => {
+    console.log('Client ready alla!')
+})
+
+// Login to DC with token
+client.login(token)
