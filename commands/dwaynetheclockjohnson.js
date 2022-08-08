fba91b8a06c7cfa634f5ed858fe2ff6af2ae2357
Justin <justin.reissig@icloud.com>
2022-08-08 03:35:18 +0200

    ok, last update on commandname

--- commands/dwaynetheclockjohnson.js
+++ commands/dwaynetheclockjohnson.js
@@ -2,7 +2,7 @@ const { SlashCommandBuilder } = require('discord.js')
 
 module.exports = {
     data: new SlashCommandBuilder()
-        .setName('cock')
+        .setName('dwaynetheclockjohnson')
         .setDescription('makes something useful or idfk lol'),
     async execute(interaction) {
         await interaction.reply('Every member of the BAH Cons. has the biggest cock on earth lol')
